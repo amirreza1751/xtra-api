23fc9d18eb6134b9758e7a70692fd9aa69a757e8
Alireza Dhb <alireza.dhb@gmail.com>
2020-06-26 01:13:47 +0430

    fix imports

--- src/main/java/com/xtra/api/controller/ChannelController.java
+++ src/main/java/com/xtra/api/controller/ChannelController.java
@@ -5,18 +5,14 @@ import com.xtra.api.model.Channel;
 import com.xtra.api.model.Server;
 import com.xtra.api.repository.ChannelRepository;
 import com.xtra.api.repository.ServerRepository;
-import com.xtra.api.specification.ChannelSpecification;
-import com.xtra.api.specification.SearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.Optional;
 
 @RestController
