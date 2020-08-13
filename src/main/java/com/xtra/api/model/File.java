49940fdc81416e78e8a54932b5e5ba9a98dd3604
Alireza Dhb <alireza.dhb@gmail.com>
2020-08-14 00:37:20 +0430

    add file list endpoint

---
+++ src/main/java/com/xtra/api/model/File.java
@@ -0,0 +1,19 @@
+package com.xtra.api.model;
+
+import lombok.Data;
+
+
+@Data
+public class File {
+    private String name;
+    private String path;
+    private long size;
+    private boolean isDirectory;
+    public File(String name, String path, long size, boolean isDirectory){
+        this.name = name;
+        this.path = path;
+        this.size = size;
+        this.isDirectory = isDirectory;
+    }
+
+}
