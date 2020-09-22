4d876491934658cdf1e06f48ab10e62821108bdd
Alireza Dhb <alireza.dhb@gmail.com>
2020-09-22 14:03:41 +0330

    add test sql file for database initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert ignore into permission (p_key,name,user_type) values ('admin_add_channel','Add Channel','ADMIN');
+insert ignore into permission (p_key,name,user_type) values ('admin_delete_channel','Delete Channel','ADMIN');
+insert ignore into permission (p_key,name,user_type) values ('admin_update_channel','Update Channel','ADMIN');
