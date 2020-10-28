66b697c466824ea577b6bc9291e75febf62c8f16
amirak <dashti.amir2752@gmail.com>
2020-10-28 19:12:10 +0330

    update StreamInfo class

--- src/main/java/com/xtra/api/model/StreamInfo.java
+++ src/main/java/com/xtra/api/model/StreamInfo.java
@@ -1,13 +1,11 @@
 package com.xtra.api.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -23,9 +21,23 @@ public class StreamInfo {
     private String videoCodec;
     private String audioCodec;
 
+    @OneToOne(mappedBy = "streamInfo")
+    @JsonBackReference
+    private Stream stream;
+
+    @Column(name = "stream_id")
+    private Long streamId;
+
     public StreamInfo() {
     }
 
+    public StreamInfo(Stream stream) {
+        this.stream = stream;
+    }
+
+    @OneToOne(mappedBy = "streamInfo", cascade = CascadeType.ALL)
+    @JsonBackReference("server_id1")
+    private StreamServer streamServer;
 }
 
 
