3cf095cdda793e351e12063f61e2f69452394ac8
amirak <dashti.amir2752@gmail.com>
2020-10-28 19:20:05 +0330

    update ProgressInfo class

--- src/main/java/com/xtra/api/model/ProgressInfo.java
+++ src/main/java/com/xtra/api/model/ProgressInfo.java
@@ -1,13 +1,11 @@
 package com.xtra.api.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -20,8 +18,22 @@ public class ProgressInfo {
     private String frameRate;
     private String bitrate;
 
+    @OneToOne(mappedBy = "progressInfo")
+    @JsonBackReference
+    private Stream stream;
+
+    @Column(name = "stream_id")
+    private Long streamId;
+
     public ProgressInfo() {
 
     }
 
+    public ProgressInfo(Stream stream) {
+        this.stream = stream;
+    }
+
+    @OneToOne(mappedBy = "progressInfo", cascade = CascadeType.ALL)
+    @JsonBackReference("server_id2")
+    private StreamServer streamServer;
 }
