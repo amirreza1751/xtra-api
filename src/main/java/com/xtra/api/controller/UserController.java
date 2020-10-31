3183d75c027eb73445036adb98fdd9f9a9df7684
Alireza dhb <Alireza.dhb@gmail.com>
2020-10-31 18:21:10 +0330

    add remove and update users endpoints

--- src/main/java/com/xtra/api/controller/UserController.java
+++ src/main/java/com/xtra/api/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.xtra.api.controller;
 
+import com.xtra.api.model.Line;
 import com.xtra.api.projection.UserInsertView;
 import com.xtra.api.projection.UserView;
 import com.xtra.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -32,5 +36,16 @@ public class UserController {
     @PostMapping("")
     public ResponseEntity<UserView> addUser(@RequestBody UserInsertView insertView) {
         return ResponseEntity.ok(userService.insert(insertView));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserView> updateUser(@PathVariable Long id, @RequestBody UserInsertView view) {
+        return ResponseEntity.ok(userService.updateOrFail(id, view));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteOrFail(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/xtra/api/service/UserService.java
+++ src/main/java/com/xtra/api/service/UserService.java
@@ -11,7 +11,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.beans.BeanUtils.copyProperties;
 
 @Service
 public class UserService extends CrudService<User, Long, UserRepository> {
@@ -46,5 +50,16 @@ public class UserService extends CrudService<User, Long, UserRepository> {
 
     public UserView getViewById(Long id) {
         return userMapper.convertToDto(super.findByIdOrFail(id));
+    }
+
+    public UserView updateOrFail(Long id, UserInsertView userView) {
+        var existingUser = findByIdOrFail(id);
+        List<String> toIgnore = new ArrayList<>();
+        toIgnore.add("id");
+        if (userView.getRoleId() == null) toIgnore.add("roleId");
+        if (userView.getPassword() == null) toIgnore.add("password");
+        else userView.setPassword(bCryptPasswordEncoder.encode(userView.getPassword()));
+        copyProperties(userMapper.convertToEntity(userView), existingUser, toIgnore.toArray(new String[0]));
+        return userMapper.convertToDto(repository.save(existingUser));
     }
 }
