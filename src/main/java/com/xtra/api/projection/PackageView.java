6a7a90926248c0a69808290d3db55ef0095da8ee
Alireza dhb <Alireza.dhb@gmail.com>
2020-11-04 12:29:55 +0330

    add naming strategy to packageView

--- src/main/java/com/xtra/api/projection/PackageView.java
+++ src/main/java/com/xtra/api/projection/PackageView.java
@@ -1,5 +1,7 @@
 package com.xtra.api.projection;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.xtra.api.model.StreamProtocol;
 import lombok.Data;
 
@@ -7,6 +9,7 @@ import java.time.Period;
 import java.util.List;
 
 @Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PackageView {
     private Long id;
     private String name;
