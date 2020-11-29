2904aff74d5bc89669218b05b2e93b4863245ca6
amirak <dashti.amir2752@gmail.com>
2020-11-29 16:11:45 +0330

    add progress info to channels which was forgot

--- src/main/java/com/xtra/api/projection/MergedChannelInfo.java
+++ src/main/java/com/xtra/api/projection/MergedChannelInfo.java
@@ -4,21 +4,22 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.xtra.api.model.ProgressInfo;
 import com.xtra.api.model.StreamInfo;
-import com.xtra.api.model.StreamType;
 import lombok.Data;
 
-import java.util.List;
 
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class ChannelInfo {
+public class MergedChannelInfo {
 
-    private Long id;
-    private String name;
-    private String logo;
-    private StreamType streamType;
-    private String streamToken;
+    private StreamInfo streamInfo;
+    private ProgressInfo progressInfo;
 
-    private List<MergedChannelInfo> infos;
+
+    public MergedChannelInfo(StreamInfo streamInfo, ProgressInfo progressInfo) {
+        this.streamInfo = streamInfo;
+        this.progressInfo = progressInfo;
+    }
+
+    public MergedChannelInfo(){}
 }
