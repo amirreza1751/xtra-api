a610a0f7eaf86ba0a565f3deb2d942fb3764c307
Alireza dhb <Alireza.dhb@gmail.com>
2020-12-16 19:28:53 +0330

    set global json naming strategy to snakeCase

--- src/main/java/com/xtra/api/ApiApplication.java
+++ src/main/java/com/xtra/api/ApiApplication.java
@@ -1,8 +1,10 @@
 package com.xtra.api;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,6 +29,13 @@ public class ApiApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
+        b.propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        return b;
     }
 
 }
