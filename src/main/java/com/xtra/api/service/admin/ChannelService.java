bf8a9f9c057ef85446c441c1b010518050742a51
Alireza dhb <Alireza.dhb@gmail.com>
2021-02-27 00:35:37 +0330

    fix stream server orphan removal

--- src/main/java/com/xtra/api/service/admin/ChannelService.java
+++ src/main/java/com/xtra/api/service/admin/ChannelService.java
@@ -1,14 +1,10 @@
 package com.xtra.api.service.admin;
 
 import com.xtra.api.exceptions.EntityNotFoundException;
+import com.xtra.api.mapper.admin.ChannelMapper;
 import com.xtra.api.mapper.admin.ChannelStartMapper;
 import com.xtra.api.model.*;
 import com.xtra.api.projection.admin.channel.*;
-import com.xtra.api.mapper.admin.ChannelMapper;
-import com.xtra.api.model.Channel;
-import com.xtra.api.model.Server;
-import com.xtra.api.model.StreamServer;
-import com.xtra.api.model.StreamServerId;
 import com.xtra.api.repository.ChannelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,9 +14,6 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.xtra.api.util.Utilities.generateRandomString;
@@ -106,19 +99,12 @@ public class ChannelService extends StreamService<Channel, ChannelRepository> {
                 if (channelMassInsertView.getRtmpOutput() != null)
                     channel.setRtmpOutput(Boolean.parseBoolean(channelMassInsertView.getRtmpOutput()));
 
-                Set<StreamServer> streamServers = channelMapper.convertToServers(serverIds);
+                Set<StreamServer> streamServers = channelMapper.convertToServers(serverIds, channelId);
                 channel.setStreamServers(streamServers);
 
-                System.out.println(channel);
+                channel.updateRelationIds();
 
                 repository.save(channel);
-
-                if (channel.getStreamServers() != null) {
-                    var resetServerIds = channel.getStreamServers().stream().map(streamServer -> streamServer.getServer().getId()).collect(Collectors.toSet());
-                    if (restart) {
-                        //@todo call stream restart on servers
-                    }
-                }
             }
         }
     }
@@ -135,10 +121,26 @@ public class ChannelService extends StreamService<Channel, ChannelRepository> {
     public Channel update(Long id, Channel channel, boolean restart) {
         //@todo check validation check
         Channel oldChannel = findByIdOrFail(id);
-        copyProperties(channel, oldChannel, "id", "currentInput", "currentConnections", "lineActivities");
-        oldChannel.setStreamInputs(channel.getStreamInputs().stream().distinct().collect(Collectors.toList()));
-        oldChannel.updateRelationIds();
+        copyProperties(channel, oldChannel, "id", "currentInput", "currentConnections", "lineActivities", "streamServers", "streamCollections");
 
+        //remove old servers from channel and add new ones
+        if (oldChannel.getStreamServers() != null) {
+            oldChannel.getStreamServers().clear();
+            oldChannel.getStreamServers().addAll(channel.getStreamServers().stream().peek(streamServer -> {
+                streamServer.setId(new StreamServerId(oldChannel.getId(), streamServer.getServer().getId()));
+                streamServer.setStream(oldChannel);
+            }).collect(Collectors.toSet()));
+        }
+        //remove old collections from channel and add new ones
+        if (oldChannel.getCollectionAssigns() != null) {
+            oldChannel.getCollectionAssigns().clear();
+            oldChannel.getCollectionAssigns().addAll(channel.getCollectionAssigns().stream().peek(collectionStream -> {
+                collectionStream.setId(new CollectionStreamId(collectionStream.getCollection().getId(), oldChannel.getId()));
+                collectionStream.setStream(oldChannel);
+            }).collect(Collectors.toSet()));
+        }
+
+        oldChannel.setStreamInputs(channel.getStreamInputs().stream().distinct().collect(Collectors.toList()));
         var savedEntity = repository.save(oldChannel);
         if (savedEntity.getStreamServers() != null) {
             var serverIds = savedEntity.getStreamServers().stream().map(streamServer -> streamServer.getServer().getId()).collect(Collectors.toSet());
