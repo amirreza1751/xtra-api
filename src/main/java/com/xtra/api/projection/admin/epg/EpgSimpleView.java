bfea7836419206b87a1e5662e8ffec558e472430
Alireza dhb <Alireza.dhb@gmail.com>
2021-02-27 01:06:45 +0330

    add id to epg dtos

--- src/main/java/com/xtra/api/projection/admin/epg/EpgSimpleView.java
+++ src/main/java/com/xtra/api/projection/admin/epg/EpgSimpleView.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class EpgSimpleView {
+    private Long id;
     private String name;
     private String source;
     private LocalDateTime lastUpdated;
