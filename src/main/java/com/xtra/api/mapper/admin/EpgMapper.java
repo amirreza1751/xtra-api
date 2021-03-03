029dcfb34471ef03e2501cf3647f7605984c0890
Alireza dhb <Alireza.dhb@gmail.com>
2021-03-03 17:02:22 +0330

    fix epgChannel details not showing

--- src/main/java/com/xtra/api/mapper/admin/EpgMapper.java
+++ src/main/java/com/xtra/api/mapper/admin/EpgMapper.java
@@ -1,16 +1,24 @@
 package com.xtra.api.mapper.admin;
 
+import com.xtra.api.model.EpgChannel;
 import com.xtra.api.model.EpgFile;
+import com.xtra.api.projection.admin.epg.EpgChannelView;
 import com.xtra.api.projection.admin.epg.EpgInsertView;
 import com.xtra.api.projection.admin.epg.EpgSimpleView;
 import com.xtra.api.projection.admin.epg.EpgView;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class EpgMapper {
     public abstract EpgSimpleView toSimpleView(EpgFile epgFile);
 
+    @Mapping(target = "channels",source = "epgChannels")
     public abstract EpgView toView(EpgFile epgFile);
 
     public abstract EpgFile toEntity(EpgInsertView epgInsertView);
+
+    @Mapping(source = "id.name",target = "name")
+    @Mapping(source = "id.language",target = "language")
+    public abstract EpgChannelView toView(EpgChannel epgChannel);
 }
