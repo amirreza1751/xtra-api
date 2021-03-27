5cd190b327e7b3af742b4a998dd164810a8d243a
Alireza dhb <Alireza.dhb@gmail.com>
2021-03-27 18:11:53 +0430

    uncomment line token generation

--- src/main/java/com/xtra/api/service/LineService.java
+++ src/main/java/com/xtra/api/service/LineService.java
@@ -51,16 +51,16 @@ public abstract class LineService extends CrudService<Line, Long, LineRepository
             line.setUsername(username);
         } else {
             if (repository.existsByUsername(lineUsername))
-                throw new RuntimeException("lineUsername already exists");
+                throw new RuntimeException("line Username already exists");
         }
         line.setPassword(bCryptPasswordEncoder.encode(line.getPassword()));
 
-        /*String token;
+        String token;
         do {
             token = generateRandomString(8, 12, false);
         }
         while (repository.findByLineToken(token).isPresent());
-        line.setLineToken(token);*/
+        line.setLineToken(token);
         return repository.save(line);
     }
 }
