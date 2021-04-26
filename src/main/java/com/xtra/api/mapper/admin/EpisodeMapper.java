0163237424f10699a7282093dba0c9ab99977a2d
amirak <dashti.amir2752@gmail.com>
2021-04-26 19:02:48 +0200

    small fix in movie, collection and category toString exceptions

--- src/main/java/com/xtra/api/mapper/admin/EpisodeMapper.java
+++ src/main/java/com/xtra/api/mapper/admin/EpisodeMapper.java
@@ -1,23 +1,8 @@
 package com.xtra.api.mapper.admin;
 
-import com.xtra.api.exception.EntityNotFoundException;
-import com.xtra.api.model.CollectionVod;
 import com.xtra.api.model.Episode;
-import com.xtra.api.model.Series;
 import com.xtra.api.projection.admin.episode.EpisodeInsertView;
-import com.xtra.api.projection.admin.series.SeriesInsertView;
-import com.xtra.api.projection.admin.series.SeriesView;
-import com.xtra.api.repository.CollectionRepository;
-import com.xtra.api.repository.CollectionVodRepository;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public abstract class EpisodeMapper {
--- src/main/java/com/xtra/api/model/CollectionVod.java
+++ src/main/java/com/xtra/api/model/CollectionVod.java
@@ -1,16 +1,17 @@
 package com.xtra.api.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(exclude = {"collection"})
 public class CollectionVod {
     @EmbeddedId
     @EqualsAndHashCode.Include
@@ -23,7 +24,7 @@ public class CollectionVod {
 
     @MapsId("collectionId")
     @ManyToOne
-    @JsonBackReference
+    @JsonManagedReference
     private Collection collection;
 
     @Column(name = "`order`")
