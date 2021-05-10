e87d57b01903c53560166ccd9e67b4dac80c14cf
Mohammadrsh <mammad.rsh@gmail.com>
2021-05-11 02:20:07 +0430

    add Advance Stream Options to Channels Batch

--- src/main/java/com/xtra/api/projection/admin/channel/ChannelBatchInsertView.java
+++ src/main/java/com/xtra/api/projection/admin/channel/ChannelBatchInsertView.java
@@ -2,6 +2,7 @@ package com.xtra.api.projection.admin.channel;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.xtra.api.model.AdvancedStreamOptions;
 import lombok.Data;
 
 import java.util.Set;
@@ -14,6 +15,24 @@ public class ChannelBatchInsertView {
     private Set<Long> channelIds;
     private Set<Long> serverIds;
     private Set<Long> collectionIds;
+
+    // null => no change | TRUE OR FALSE TO CHANGE
+    private String generatePts;
+    private String nativeFrames;
+    private String streamAllCodecs;
+    private String allowRecording;
+    private String outputRTMP;
+    private String directSource;
+
+    // null => no change | EMPTY STRING TO CLEAR, FILL TO SAVE
+    private String customChannelSID;
+    private String onDemandProbeSize;
+    private String minuteDelay;
+    private String userAgent;
+    private String httpProxy;
+    private String cookie;
+    private String headers;
+    private String transcodingProfile;
 
     private Boolean keepServers;
     private Boolean keepCollections;
