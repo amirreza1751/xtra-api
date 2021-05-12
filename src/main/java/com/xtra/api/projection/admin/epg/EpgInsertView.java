8e079669efab8ba6b5e94840bc18b43a6ef68596
Sepehr <sepehr@boojan.co>
2021-05-12 18:46:41 +0430

    episode validations done

--- src/main/java/com/xtra/api/projection/admin/epg/EpgInsertView.java
+++ src/main/java/com/xtra/api/projection/admin/epg/EpgInsertView.java
@@ -2,8 +2,12 @@ package com.xtra.api.projection.admin.epg;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class EpgInsertView {
+    @NotNull(message = "EPG name can not be empty")
     private String name;
+    @NotNull(message = "EPG source can not be empty")
     private String source;
 }
--- src/main/java/com/xtra/api/projection/admin/episode/EpisodeInsertView.java
+++ src/main/java/com/xtra/api/projection/admin/episode/EpisodeInsertView.java
@@ -2,19 +2,24 @@ package com.xtra.api.projection.admin.episode;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.xtra.api.model.SeriesInfo;
 import com.xtra.api.projection.admin.season.SeasonInsertView;
 import com.xtra.api.projection.admin.video.VideoInsertView;
 import lombok.Data;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
+
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class EpisodeInsertView {
+    @NotNull(message = "Episode number can not be empty")
+    @Digits(message = "Episode number should be a 3 digits number", integer = 3, fraction = 0)
     private int episodeNumber;
+    @NotBlank(message = "Episode name can not be empty")
     private String episodeName;
     private String notes;
     private String imageUrl;
@@ -23,6 +28,8 @@ public class EpisodeInsertView {
     private int runtime;
     private float rating;
 
+    @NotNull(message = "season can not be empty")
     private SeasonInsertView season;
+    @NotNull(message = "videos list can not be empty")
     private List<VideoInsertView> videos;
 }
