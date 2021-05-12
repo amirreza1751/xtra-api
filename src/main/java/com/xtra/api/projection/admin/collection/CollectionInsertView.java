fef4f2745b92e9c3ac61651a615112e2c3f01b15
Sepehr <sepehr@boojan.co>
2021-05-12 18:36:24 +0430

    validations for collection and download done

--- src/main/java/com/xtra/api/projection/admin/collection/CollectionInsertView.java
+++ src/main/java/com/xtra/api/projection/admin/collection/CollectionInsertView.java
@@ -5,14 +5,19 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.xtra.api.model.MediaType;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CollectionInsertView {
     private Long id;
+    @NotBlank(message = "collection name can not be empty")
     private String name;
+    @NotBlank(message = "collection category can not be empty")
     private String categoryName;
+    @NotNull(message = "collection type can not be empty")
     private MediaType type;
     private List<Long> channels;
     private List<Long> movies;
