d6903c248d7e3021afa200fa69c2a0eec64e9a48
Alireza dhb <Alireza.dhb@gmail.com>
2021-08-25 20:59:51 +0430

    improve geoip update

--- src/main/java/com/xtra/api/service/admin/GeoIpService.java
+++ src/main/java/com/xtra/api/service/admin/GeoIpService.java
@@ -4,7 +4,6 @@ import com.maxmind.db.CHMCache;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
-import com.maxmind.geoip2.record.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Optional;
 
 @Service
@@ -22,7 +20,7 @@ public class GeoIpService {
 
     @Autowired
     public GeoIpService(@Value("${maxmind.dbPath}") String dbPath) {
-        File database = new File(System.getProperty("user.home") + dbPath);
+        File database = new File(dbPath);
         try {
             this.reader = new DatabaseReader.Builder(database).withCache(new CHMCache()).build();
         } catch (IOException e) {
