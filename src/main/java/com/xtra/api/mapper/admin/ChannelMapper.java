65b5990833b9c2e2cbb814e43e924d7d04185006
Mohammadrsh <mammad.rsh@gmail.com>
2021-10-28 13:14:26 +0200

    add link to stream

--- src/main/java/com/xtra/api/mapper/admin/ChannelMapper.java
+++ src/main/java/com/xtra/api/mapper/admin/ChannelMapper.java
@@ -8,11 +8,16 @@ import com.xtra.api.model.collection.CollectionStreamId;
 import com.xtra.api.model.epg.EpgChannel;
 import com.xtra.api.model.exception.EntityNotFoundException;
 import com.xtra.api.model.stream.*;
+import com.xtra.api.model.vod.Movie;
 import com.xtra.api.projection.admin.channel.*;
 import com.xtra.api.projection.admin.epg.EpgDetails;
+import com.xtra.api.projection.admin.movie.MovieListView;
+import com.xtra.api.projection.admin.movie.MovieServerInfo;
+import com.xtra.api.projection.admin.movie.MovieVideoInfo;
 import com.xtra.api.repository.*;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
@@ -44,6 +49,14 @@ public abstract class ChannelMapper {
     private ConnectionRepository connectionRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private LineRepository lineRepository;
+
+    @Value("${server.external.address}")
+    private String serverAddress;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @Mapping(target = "streamServers", ignore = true)
     @Mapping(target = "collectionAssigns", ignore = true)
@@ -195,6 +208,13 @@ public abstract class ChannelMapper {
     @Mapping(source = "channel", target = "totalUsers", qualifiedByName = "getTotalUsers")
     @Mapping(target = "epg", expression = "java(channel.getEpgChannel()!=null)")
     public abstract ChannelInfo convertToChannelInfo(Channel channel);
+
+    @AfterMapping
+    public void assignLink(final Channel channel, @MappingTarget ChannelInfo channelInfo) {
+        var system_line = lineRepository.findByUsername("system_line");
+        String link = system_line.map(line -> "http://" + serverAddress + ":" + serverPort + "/api/play/channel/" + line.getLineToken() + "/" + channel.getStreamToken()).orElse("");
+        channelInfo.setLink(link);
+    }
 
     @Named("getTotalUsers")
     long getTotalUsers(Channel channel) {
